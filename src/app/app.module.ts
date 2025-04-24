53833537cff53b9aeaddeb3524336b3bc5eabb2f
GuilhermeSerafim <guilerstudies@gmail.com>
2025-04-24 15:08:36 -0300

    Criando meu primeiro componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent // Referenciando componente - Precisamos declarar os componentes que fomos usar no módulo
   ],
   imports: [
     BrowserModule
