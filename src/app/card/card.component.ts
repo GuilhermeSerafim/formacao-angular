d4fedcdb2a1a2e7a5dfc71d9c26a5838d57183d3
GuilhermeSerafim <guilerstudies@gmail.com>
2025-04-24 16:54:07 -0300

    Simulando comportamento reativo com programação imperativa

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -9,7 +9,27 @@ export class CardComponent {
   tipo = 'Simples'
   preco = 100
 
+
+  numero: number = 0;
+
   getFullPrice() {
     return `R$${this.preco},00/Mês`
+  }
+
+
+  ngOnInit(): void {
+    this.contarAte(10);
+  }
+
+  contarAte(limite: number): void {
+    const timer = setInterval(() => {
+      this.numero++;
+      console.log(`Valor atual: ${this.numero}`);
+
+      if (this.numero >= limite) {
+        clearInterval(timer);
+        console.log('Contagem finalizada.');
+      }
+    }, 1000); // atualiza de 1 em 1 segundo
   }
 }
