98ef5cba62cc4c287736b6b268216fee556eff30
GuilhermeSerafim <guilerstudies@gmail.com>
2025-04-24 16:23:14 -0300

    Utilizando componentes de modulo x em modulo y e entendendo o funcionamento disto

---
+++ src/app/cards/cards.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardComponent } from '../card/card.component';
+import { CardRoxoComponent } from '../card-roxo/card-roxo.component';
+import { CardButtonComponent } from '../card-button/card-button.component';
+import { CardRoxoButtonComponent } from '../card-roxo-button/card-roxo-button.component';
+
+@NgModule({
+  declarations: [
+    CardComponent,
+    CardRoxoComponent,
+    CardButtonComponent,
+    CardRoxoButtonComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  // Especificando todos os componentes disponiveis, caso alguém importe este módulo
+  exports: [
+    CardComponent,
+    CardRoxoComponent,
+    CardButtonComponent,
+    CardRoxoButtonComponent
+  ]
+})
+export class CardsModule { }
