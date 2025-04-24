9a17ad96c6a46456bf831b9c75d4d6f618410b4c
GuilhermeSerafim <guilerstudies@gmail.com>
2025-04-24 17:54:22 -0300

    Entendendo interfaces, usando em object

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 
-@Component({
+interface IPlano {
+  infos: IInfos;
+}
+
+interface IInfos {
+  tipo: string;
+  preco: number;
+}
+
+@Component ({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
+
 export class CardComponent {
   // Com any, você está dizendo: "A variável plano pode ser qualquer coisa (objeto, string, número, função...)".
-  plano: any = {
-    infos: undefined
+  plano: IPlano = {
+    infos: {
+      tipo:  'Simples',
+      preco: 100,
+    }
   }
 }
