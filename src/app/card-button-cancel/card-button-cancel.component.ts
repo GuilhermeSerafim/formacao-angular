8ceeb4198af2f9fb12fa373660456a0819498cbf
GuilhermeSerafim <guilerstudies@gmail.com>
2025-04-25 15:23:00 -0300

    Sobrepondo estilos

---
+++ src/app/card-button-cancel/card-button-cancel.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-button-cancel',
+  templateUrl: './card-button-cancel.component.html',
+  styleUrls: ['./card-button-cancel.component.scss', './card-button-cancel.component2.scss'] // Sobrepopondo
+})
+export class CardButtonCancelComponent {
+
+}
