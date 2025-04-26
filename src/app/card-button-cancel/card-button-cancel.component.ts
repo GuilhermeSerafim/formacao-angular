bb137e3f2f5cec914e7657028dfc94a522225043
GuilhermeSerafim <guilerstudies@gmail.com>
2025-04-26 12:24:38 -0300

    Visualizando outros comportamentos do shadow dom

--- src/app/card-button-cancel/card-button-cancel.component.ts
+++ src/app/card-button-cancel/card-button-cancel.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card-button-cancel',
   templateUrl: './card-button-cancel.component.html',
-  styleUrls: ['./card-button-cancel.component.scss', './card-button-cancel.component2.scss'] // Sobrepopondo
+  styleUrls: ['./card-button-cancel.component.scss'] // Sobrepopondo
 })
 export class CardButtonCancelComponent {
 
