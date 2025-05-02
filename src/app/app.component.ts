57652b5398f796e9f03cfad67654aff6dab649e0
GuilhermeSerafim <guilerstudies@gmail.com>
2025-05-02 16:05:07 -0300

    Realizando desafio proposto na aula - modularizando objetos com props dinamicas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+interface IPlano {
+  tipo: 'Simples' | 'Premium' | 'Avançado'; // restringe valores possíveis
+  preco: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,10 +10,19 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  planoSimples: IPlano = {
+    preco: 100,
+    tipo: "Simples"
+  }
+  planoPremium: IPlano = {
+    preco: 300,
+    tipo: "Premium"
+  }
+  planoAvancado: IPlano = {
+    preco: 400,
+    tipo: "Avançado"
+  }
   onCardButtonClicked() {
     console.log("Simulando chamada HTTP")
-  }
-  buttonClicked() {
-    console.log("clicado")
   }
 }
