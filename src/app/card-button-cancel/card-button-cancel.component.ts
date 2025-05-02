9dbe39b3219379305848a427ae0c2d889a233e45
GuilhermeSerafim <guilerstudies@gmail.com>
2025-05-02 14:00:20 -0300

    Vendo outras formas de usar o transform

--- src/app/card-button-cancel/card-button-cancel.component.ts
+++ src/app/card-button-cancel/card-button-cancel.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { booleanAttribute, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-button-cancel',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-button-cancel.component.scss'] // Sobrepopondo
 })
 export class CardButtonCancelComponent {
-
+  // Verifica se a propriedade está sendo referenciada - booleanAttribute
+  // tipoAttribute também é usado para conversões - evite tantos transforms...
+  @Input({transform: booleanAttribute}) buttonDisabled: boolean = false;
 }
