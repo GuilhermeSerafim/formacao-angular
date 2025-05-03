3e5f44099c3db4ac5de6646df9c2cf4ad4bcc488
GuilhermeSerafim <guilerstudies@gmail.com>
2025-05-03 17:27:46 -0300

    Realizando style binding em elementos com indice impares

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'prj-angular';
-  listPessoas = [
+  listPeople = [
     {
       name: 'Felipe Freitas',
       age: '26 anos'
