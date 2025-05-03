dac1dda5071d6904c620dd94b5f331d6ecee3edc
GuilhermeSerafim <guilerstudies@gmail.com>
2025-05-03 17:05:33 -0300

    Entendendo primeiramente como o ng for funciona

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,4 @@
 import { Component } from '@angular/core';
-interface IPlano {
-  tipo: 'Simples' | 'Premium' | 'Avançado'; // restringe valores possíveis
-  preco: number;
-}
-interface IPessoa {
-  nome?: string,
-  idade: number,
-}
 
 @Component({
   selector: 'app-root',
@@ -14,10 +6,7 @@ interface IPessoa {
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  prop = true;
-  toggleDiv() {
-    this.prop = !this.prop;
-  }
   title = 'prj-angular';
-
+  listPessoas = ['Felipe', 'João', true, 1000];
+  
 }
