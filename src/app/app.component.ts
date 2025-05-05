f1663e90da729840aec3683328d50fb9bd8f1791
GuilhermeSerafim <guilerstudies@gmail.com>
2025-05-05 18:07:39 -0300

    Entendendo class binding e ngClass e suas diferenças

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,51 +6,14 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-
-
+  setToGreen() {
+    this.IsGreen = true
+  }
+  setToOrange() {
+    this.IsGreen = false
+  }
   title = 'prj-angular';
-  personSelectedIndex: number = -1;
-  listPeople = [
-    {
-      name: 'Felipe Freitas',
-      age: '26 anos'
-    },
-    {
-      name: 'Jake Paul',
-      age: '34 anos'
-    },
-    {
-      name: 'Mike Tyson',
-      age: '55 anos'
-    },
-    {
-      name: 'Noir Walk',
-      age: '20 anos'
-    },
-  ];
-  stylesH1 = "fontSize: 15px; backgroundColor: red;";
-
-  selectedPerson(i: number) {
-    this.personSelectedIndex = i;
-  }
-
-  fontSize: number = 15;
-  textColor: 'purple' | 'orange' = 'purple';
-  buttonText: 'Roxo' | 'Laranja' = 'Roxo'
-  encriseFontSize() {
-    this.fontSize += 1;
-    // Forma mais verbosa, portanto, se você for usar um objeto, não use, neste caso, se for manipular o mesmo!
-    this.stylesH1 = `fontSize: ${this.fontSize}px; backgroundColor: red;`
-  }
-
-  toogleFontColor() {
-    if (this.textColor == "purple") {
-      this.textColor = 'orange'
-      this.buttonText = 'Laranja'
-    } else {
-      this.textColor = 'purple'
-      this.buttonText = 'Roxo'
-    }
-  }
-
+  IsGreen = true;
 }
+
+
