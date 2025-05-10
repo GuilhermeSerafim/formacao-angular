23e15d02e07d822cc2e443de91559f88641a5d22
GuilhermeSerafim <guilerstudies@gmail.com>
2025-05-10 17:28:04 -0300

    Criando pipe para exibir endereço formatado

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PhonePipe } from './phone.pipe';
+import { AddressPipe } from './address.pipe';
 
 
 
 @NgModule({
   declarations: [
     PhonePipe,
+    AddressPipe,
   ],
   imports: [
     CommonModule,
   ],
   exports: [
     PhonePipe,
+    AddressPipe,
   ]
 })
 export class PipesModule { }
