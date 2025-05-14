07c22ed091a7d89d60f86f17c0bb5aaf75ae8df5
GuilhermeSerafim <guilerstudies@gmail.com>
2025-05-14 16:48:19 -0300

    Externalizando funções de filtros em utils

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { UsersList } from './data/users-list';
 import { IUser } from './interfaces/user/user.interface';
 import { IFilterOptions } from './interfaces/filter-options';
 import { isWithinInterval } from 'date-fns';
+import filterUsersList from './utils/filter-users-list';
 
 @Component({
   selector: 'app-root',
@@ -32,48 +33,7 @@ export class AppComponent implements OnInit {
   }
 
   onFilter(filterOptions: IFilterOptions) {
-    this.usersListFiltered = this.filterUsersList(filterOptions, this.usersList);
-  }
-
-  // Pure Functional Programming
-  filterUsersList(filterOptions: IFilterOptions, usersList: IUser[]): IUser[] {
-    let filteredList: IUser[] = [];
-
-    // Boa prática - É melhor pegarmos o parametro do que puxar mais uma vez a varíavel externa
-    filteredList = this.filteredUsersListByName(filterOptions.name, usersList);
-    // Construindo cascata
-    filteredList = this.filteredUsersListByStatus(filterOptions.status, filteredList);
-    filteredList = this.filterUsersListByDate(filterOptions.startDate, filterOptions.endDate, filteredList);
-
-    return filteredList;
-  }
-
-  filteredUsersListByName(name: string | undefined, usersList: IUser[]): IUser[] {
-    const NAME_NOT_TYPPED = name === undefined;
-
-    if (NAME_NOT_TYPPED) return usersList;
-
-    return usersList.filter((user) => user.name.toLowerCase().includes(name.toLocaleLowerCase()));
-  }
-
-  filteredUsersListByStatus(status: boolean | undefined, usersList: IUser[]): IUser[] {
-    const STATUS_NOT_SELECTED = status === undefined;
-
-    if (STATUS_NOT_SELECTED) return usersList;
-
-    return usersList.filter((user) => user.isActive === status);
-  }
-
-  // Estamos chamando de userList o 3ª parametro, pois não importa para a função se está filtrada ou não 🤷‍♀️
-  filterUsersListByDate(startDate: Date | undefined, endDate: Date | undefined, userList: IUser[]): IUser[] {
-    const DATES_NOT_SELECTED = startDate === undefined || endDate === undefined;
-
-    if (DATES_NOT_SELECTED) return userList;
-
-    return userList.filter((user) => isWithinInterval(new Date(user.registrationDate), {
-      start: startDate, // Include
-      end: endDate // Exclude
-    }));
+    this.usersListFiltered = filterUsersList(filterOptions, this.usersList);
   }
 }
 
