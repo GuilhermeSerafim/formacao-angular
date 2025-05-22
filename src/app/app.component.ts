1381b6defabfa4086c1b6096f35890ddda97b814
GuilhermeSerafim <guilerstudies@gmail.com>
2025-05-22 16:48:20 -0300

    Aprendendo superficialmente sobre lifecycles, como funciona OnInit, AfterViewInit, constructor (em sequencia de renderização)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FilhoComponent } from './filho/filho.component';
 
 @Component({
@@ -6,13 +6,27 @@ import { FilhoComponent } from './filho/filho.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'prj-angular';
-  // É mais recomendável utilizar o @input e @output para deixar melhor documentado, dificilemnte vc vai utilizar isto
-  @ViewChild(FilhoComponent) filhoCompRef!: FilhoComponent;
-  hello() {
-    this.filhoCompRef.dizerOi();
-    this.filhoCompRef.message = 'Oi atualizado';
+  @ViewChild('meuInput') meuInputEl!: ElementRef<HTMLInputElement>;
+
+  // • Primeiro o Angular instancia a classe do componente, faz a injeção de dependências e inicializa propriedades padrão.
+  // • Aqui ainda não existem dados de entrada (inputs) nem o template renderizado.
+  constructor() {
+    console.log("constructor")
+  }
+
+  // • É chamado logo após o Angular ter atribuído os valores de todos os @Input() e executado a primeira detecção de mudanças.
+  // • Use-o para inicializar lógica que dependa de propriedades configuradas pelo pai.
+  ngOnInit(): void {
+    console.log("oninit")
+  }
+
+  // • Só é disparado depois que o Angular renderizou o template e criou todas as views filhas(e variáveis de referência como @ViewChild).
+  // • Aqui já dá para acessar com segurança o this.meuInputEl.nativeElement e, por exemplo, dar foco no input.
+  ngAfterViewInit(): void {
+    console.log("afterview")
+    this.meuInputEl.nativeElement.focus();
   }
 }
 
