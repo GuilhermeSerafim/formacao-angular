00b0cdc9a2097b05f5a1addb001f603ea4018e42
GuilhermeSerafim <guilerstudies@gmail.com>
2025-05-22 14:40:55 -0300

    Criação, utilização do template variable e entendimento do change detection do Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'prj-angular';
+  binding: string = "Guiler";
+
+  constructor(
+    private _cdRef: ChangeDetectorRef
+  ) { }
+
+  changeDetection() {
+    this._cdRef.detectChanges();
+  }
 }
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { ComponentsModule } from './components/components.module';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,12 +11,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    ComponentsModule,
     BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [
     provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+  export class AppModule { }
