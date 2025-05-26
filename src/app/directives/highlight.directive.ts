f72038954ff63d7be2061130620fd428ec2e4da6
GuilhermeSerafim <guilerstudies@gmail.com>
2025-05-26 09:42:16 -0300

    Criando diretiva de atributo, usando hostbinding em propriedades e hostlistener para escutar eventos e aplicar determinado comportamento na diretiva

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -1,8 +1,20 @@
-import { Directive } from "@angular/core";
+import { Directive, HostBinding, HostListener } from "@angular/core";
 
 @Directive({
     selector: "[appHighlight]",
 })
+// DIRETIVAS (ESSA É ATRIBUTO):
 export class HighlightDirective {
+    // Estruturais (*ngIf, *ngFor, etc.) → DOM: adicionam/removem/iteram elementos → atuam como fluxo de controle na view.
+    // Atributo ([ngClass], [ngStyle], etc.) → elemento existente: mudam estilo/comportamento sem alterar sua existência.
+    @HostBinding('style.backgroundColor') bgColor = 'transparent';
+
+    @HostListener('mouseover') onMouseOver() {
+        this.bgColor = 'purple'
+    }
+
+    @HostListener('mouseout') onMouseOut() {
+        this.bgColor = 'transparent';
+    }
 
 }
