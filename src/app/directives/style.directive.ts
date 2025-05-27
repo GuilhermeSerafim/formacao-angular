588f81766a2b11d9da2b807abb7881ae09ec976a
GuilhermeSerafim <guilerstudies@gmail.com>
2025-05-27 10:09:20 -0300

    Modificando estilo de propriedades com hosting binding

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,8 +1,11 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appStyle]'
 })
 export class StyleDirective {
-  
+  @HostBinding('style') paragrafoEstilizado = {
+    backgroundColor: 'purple',
+    color: 'yellow'
+  }   
 }
