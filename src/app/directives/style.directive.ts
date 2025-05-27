a916fe0bb39fcabdbfbf231ba679c5b326c3d5e7
GuilhermeSerafim <guilerstudies@gmail.com>
2025-05-27 10:25:21 -0300

    Modificando classes com hosting binding, e estudando propriedade style (criado pelo Angular)

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -7,5 +7,6 @@ export class StyleDirective {
   @HostBinding('style') paragrafoEstilizado = {
     backgroundColor: 'purple',
     color: 'yellow'
-  }   
+  }
+  @HostBinding('class') paragrafoComClasse = ['meu-texto', 'font-size', 'padding'];
 }
