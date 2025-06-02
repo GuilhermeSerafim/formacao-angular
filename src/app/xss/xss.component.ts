d2deda712c6f486b6cc23ebdfc21f96c4de93b26
GuilhermeSerafim <guilerstudies@gmail.com>
2025-06-02 13:40:10 -0300

    Usando renderer2 como tratativa do XSS

--- src/app/xss/xss.component.ts
+++ src/app/xss/xss.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, input } from '@angular/core';
+import { Component, ElementRef, input, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-xss',
@@ -6,12 +6,27 @@ import { Component, ElementRef, input } from '@angular/core';
   styleUrl: './xss.component.scss'
 })
 export class XssComponent {
-  constructor(private readonly _elRef: ElementRef){}
+
+  constructor(
+    private readonly _elRef: ElementRef,
+    private readonly _renderer2: Renderer2
+  ) { }
+
   createElement(inputText: string) {
     const divEl = document.createElement('div') as HTMLDivElement;
     divEl.innerHTML = inputText;
     divEl.style.backgroundColor = 'red'
     this._elRef.nativeElement.appendChild(divEl);
   }
+
+
+  createElementCorrect(inputText: string) {
+    const divEl = this._renderer2.createElement('div');
+    const text = this._renderer2.createText(inputText);
+    // inserindo texto dentro da divEl
+    this._renderer2.appendChild(divEl, text);
+    // inserindo divEl dentro do nosso template pelo ElementRef
+    this._renderer2.appendChild(this._elRef.nativeElement, divEl);
+  } 
 
 }
