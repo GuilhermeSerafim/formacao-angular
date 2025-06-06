459b0da629ad316fd17e6d8fd4a71f68b7c44ed5
GuilhermeSerafim <guilerstudies@gmail.com>
2025-06-06 16:46:10 -0300

    Componente client-infos-card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { CardContentComponent } from './components/card-content/card-content.com
 import { BalanceCardComponent } from './components/balance-card/balance-card.component';
 import { CreditCardComponent } from './components/credit-card/credit-card.component';
 import { DebitCardComponent } from './components/debit-card/debit-card.component';
+import { ClientInfosCardComponent } from './components/client-infos-card/client-infos-card.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -19,6 +20,7 @@ import { DebitCardComponent } from './components/debit-card/debit-card.component
     BalanceCardComponent,
     CreditCardComponent,
     DebitCardComponent,
+    ClientInfosCardComponent,
   ],
   imports: [
     BrowserModule,
