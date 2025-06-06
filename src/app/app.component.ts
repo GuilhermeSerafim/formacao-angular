10375b4666c0276116562e21230d6de91830910b
GuilhermeSerafim <guilerstudies@gmail.com>
2025-06-05 21:30:40 -0300

    Realizando uma parte do desafio de componentizar uma aplicação que criei - section-8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { LinhaCard } from './interfaces/linha-card';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, Vie
 })
 export class AppComponent {
   title = 'prj-angular';
+
+  itensSaldosDisponiveisMockado: LinhaCard[] = [
+    { chave: 'Conta Corrente', valor: 'R$ 200,00', line: true },
+    { chave: 'Conta Corrente Saldo Vinculados', valor: 'R$ 300,00', line: true },
+    { chave: 'Conta Poupança Saldo Vinculados', valor: 'R$ 400,00', line: true },
+    { chave: 'Investimentos com Baixa Automática', valor: 'R$ 500,00', line: true },
+    { chave: 'Investimentos sem Baixa Automática', valor: 'R$ 500,00', line: true }
+  ]
 }
