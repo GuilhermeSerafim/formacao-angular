b71b27398be45c7f96b48ce9f76833e86039db31
GuilhermeSerafim <guilerstudies@gmail.com>
2025-06-12 14:53:04 -0300

    Limpando para dar inicio a nova seção sobre projeção de conteudos e estilização de diretivas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,9 @@ import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
-import { CardComponent } from './components/card/card.component';
-import { CardHeaderComponent } from './components/card-header/card-header.component';
-import { CardContentComponent } from './components/card-content/card-content.component';
-import { BalanceCardComponent } from './components/balance-card/balance-card.component';
-import { CreditCardComponent } from './components/credit-card/credit-card.component';
-import { DebitCardComponent } from './components/debit-card/debit-card.component';
-import { ClientInfosCardComponent } from './components/client-infos-card/client-infos-card.component';
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent,
-    CardHeaderComponent,
-    CardContentComponent,
-    BalanceCardComponent,
-    CreditCardComponent,
-    DebitCardComponent,
-    ClientInfosCardComponent,
   ],
   imports: [
     BrowserModule,
