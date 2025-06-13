0ede077e748283ab0ae807f240c45c30052da46e
GuilhermeSerafim <guilerstudies@gmail.com>
2025-06-13 18:44:49 -0300

    Modificando texto com diretiva de seletor de elemento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { CardComponent } from './components/card/card.component';
+import { CardTextTitleDirective } from './directives/card-text-title.directive';
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
+    CardTextTitleDirective,
   ],
   imports: [
     BrowserModule,
