a09210b3054dd95aa5e4e7a1daad99e8dd62c5a3
GuilhermeSerafim <guilerstudies@gmail.com>
2025-06-15 17:43:30 -0300

    Aplicação de diretivas em card de projeção

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { CardComponent } from './components/card/card.component';
 import { CardTextTitleDirective } from './directives/card-text-title.directive';
+import { CardHeaderDirective } from './components/card/directives/card-header.directive';
+import { CardContentDirective } from './components/card/directives/card-content.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
     CardTextTitleDirective,
+    CardHeaderDirective,
+    CardContentDirective,
   ],
   imports: [
     BrowserModule,
