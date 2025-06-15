6decf912ac95691b954b4caca19e1a1e38d25d99
GuilhermeSerafim <guilerstudies@gmail.com>
2025-06-14 21:47:27 -0300

    Utilizando host binding com input em uma direitva com seletor tag para modificação da cor

--- src/app/directives/card-text-title.directive.ts
+++ src/app/directives/card-text-title.directive.ts
@@ -1,8 +1,10 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: 'app-card-text-title',
   host: { "class": 'meu-texto' },
 })
 export class CardTextTitleDirective {
+  @Input()
+  @HostBinding('style.color') color: string = '';
 }
