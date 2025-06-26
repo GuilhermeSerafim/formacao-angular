9b1e4c99a4708695f93bbbc4c7c869a7dd2533dc
Guilherme Serafim <guilerstudies@gmail.com>
2025-06-26 11:13:43 -0300

    feat: aplicar modificadores de cor via HostBinding isolado na diretiva

--- src/app/components/card/directives/card-header.directive.ts
+++ src/app/components/card/directives/card-header.directive.ts
@@ -1,7 +1,19 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding, Input } from '@angular/core';
 
+// Se você quer controle, escalabilidade e clareza no uso — continue com a diretiva.
+// Se quiser flexibilidade visual total e abertura para skin, temas ou overrides CSS, talvez classes CSS tenham mais fôlego a longo prazo.
 @Directive({
   selector: 'app-card-header',
-  host: { 'class': 'ca-c-card__header' }
+  host: { 'class': 'ca-c-card__header'}
 })
-export class CardHeaderDirective {}
+export class CardHeaderDirective {
+  @Input() bgColor: "primmary" | "secondary" | "error" = "primmary"; 
+  @HostBinding('style.backgroundColor')
+  get cardBgColor(): string {
+    return {
+      secondary: "#32e9da",
+      error: "red",
+      primmary: "rgb(233, 129, 50)"  
+    }[this.bgColor];
+  } 
+}
