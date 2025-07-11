866333fd2cfbf91c4705f09bd756f4f049307c64
Guilherme Serafim <guilerstudies@gmail.com>
2025-07-10 21:14:30 -0300

    Entendendo diferença de elemento cru, ng container e ng template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+import { MeuCompComponent } from './components/meu-comp/meu-comp.component';
 @NgModule({
   declarations: [
     AppComponent,
+    MeuCompComponent
   ],
   imports: [
     BrowserModule,
