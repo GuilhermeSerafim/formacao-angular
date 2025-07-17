e56b9f849226083ba8d02eaafbb37019009c3fe8
Guilherme Serafim <guilerstudies@gmail.com>
2025-07-17 14:33:13 -0300

    Usando AfterContentInit para projeção externa, pois ele é chamado somente quando o conteúdo externo é carregado - Ciclo de vida de componentes.

--- src/app/components/card-template-content/card-template-content.component.ts
+++ src/app/components/card-template-content/card-template-content.component.ts
@@ -1,10 +1,19 @@
-import { Component, ContentChild, TemplateRef } from '@angular/core';
+import { AfterContentInit, Component, ContentChild, OnInit, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'app-card-template-content',
   templateUrl: './card-template-content.component.html',
   styleUrl: './card-template-content.component.scss'
 })
-export class CardTemplateContentComponent {
+export class CardTemplateContentComponent implements OnInit, AfterContentInit {
   @ContentChild('meuTemplate') templateContent!: TemplateRef<any>;
+
+  // ngOnInit(): é chamado assim que o componente é inicializado, antes do conteúdo filho ser projetado.
+  // ngAfterContentInit(): é chamado depois que o conteúdo externo (projetado com ng-content ou @ContentChild) é completamente carregado e disponível no componente.
+  ngOnInit(): void {
+    console.log('OnInit', this.templateContent);
+  }
+  ngAfterContentInit(): void {
+    console.log('AfterContentInit', this.templateContent);
+  }
 }
