9e165380be4518c652e11bcb1fa9dfb0246563cb
Guilherme Serafim <guilerstudies@gmail.com>
2025-07-23 14:02:46 -0300

    refactor: padronização com interface tipada e código mais legível no pipe de status

--- src/app/pipes/user-status.pipe.ts
+++ src/app/pipes/user-status.pipe.ts
@@ -1,13 +1,17 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { UserStatusEnum } from '../app.component';
+import { IUserStatusDescription } from '../interfaces/iuser-status-description';
 
 @Pipe({
   name: 'userStatus',
 })
 export class UserStatusPipe implements PipeTransform {
-  transform(status: number): string {
-    if (status == UserStatusEnum.ATIVO) return 'Ativo';
-    else if (status == UserStatusEnum.INATIVO) return 'Inativo';
-    else return 'Inválido';
+  transform(userStatus: number): string {
+    const userStatusDescription: IUserStatusDescription = {
+      [UserStatusEnum.ATIVO]: 'Ativo',
+      [UserStatusEnum.INATIVO]: 'Inativo',
+    };
+
+    return userStatusDescription[userStatus] ?? 'Inválido';
   }
 }
