5ec3d8b427e3613af077f10fc5353abeb4355a26
Guilherme Serafim <guilerstudies@gmail.com>
2025-07-23 21:15:08 -0300

    feat: mostra data ISO 8601 convertida para fuso local usando pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
-import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
 
 export enum UserStatusEnum {
   ATIVO = 1,
@@ -10,10 +18,12 @@ export enum UserStatusEnum {
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  user = {
-    nome: 'Felipe',
-    idade: 26,
-    status: UserStatusEnum.ATIVO,
+export class AppComponent implements OnInit{
+  // minhaData: Date-> INCORRETO! = '2023-10-21T21:00:00.000Z';
+  // Quando a data vem nesse formato, não coloca Date, pois o Date é do JS, e esse formato é https://en.wikipedia.org/wiki/ISO_8601
+  minhaData: string = '2023-10-21T21:00:00.000Z';
+  minhaData2: string = '2023-10-21T21:00:00.000';
+  ngOnInit(): void {
+    console.log('', new Date(this.minhaData));
   }
 }
