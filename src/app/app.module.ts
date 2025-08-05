60c0c5c7638013a99c70e6041caab0ee4d227770
Guilherme Serafim <guilerstudies@gmail.com>
2025-08-05 12:32:31 -0300

    Configurando localizadade do date pipe para mostrar em português a data formatada GLOBALMENTE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -19,6 +19,10 @@ registerLocaleData(localePt, 'pt-BR');
     {
       provide: DATE_PIPE_DEFAULT_OPTIONS,
       useValue: { dateFormat: "'Data: 'dd/MM/YYYY", timezone: '+0000' },
+    },
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt-BR',
     },
   ],
   bootstrap: [AppComponent],
