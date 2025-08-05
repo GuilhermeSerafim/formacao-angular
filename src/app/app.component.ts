fdc8050c476bc39199d6748f534ac8f486f63fb4
Guilherme Serafim <guilerstudies@gmail.com>
2025-08-05 15:46:43 -0300

    Entendendo como funciona a quantidade minima programada no decimal pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,5 @@
 import {
-  AfterViewInit,
   Component,
-  ElementRef,
-  OnInit,
-  QueryList,
-  ViewChild,
-  ViewChildren,
 } from '@angular/core';
 
 export enum UserStatusEnum {
@@ -18,18 +12,5 @@ export enum UserStatusEnum {
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit {
-  // minhaData: Date-> INCORRETO! = '2023-10-21T21:00:00.000Z';
-  // Quando a data vem nesse formato, não coloca Date, pois o Date é do JS, e esse formato é https://en.wikipedia.org/wiki/ISO_8601
-  minhaData: string = '2023-10-21T21:00:00.000Z'; // Z == conversão para região
-  minhaData2: string = '2023-10-21T21:00:00.000';
-  // Convertendo na instancia
-  minhaDataObj: Date = new Date('2023-10-21T21:00:00.000Z');
-  ngOnInit(): void {
-    console.log('Data convertida para região', new Date(this.minhaData));
-    console.log('Data UTC-0', new Date(this.minhaData).toUTCString());
-    // A conversão regional, foi feita no new Date e não no date pipe
-    console.log('Timestamp:', new Date(this.minhaData).getTime());
-    console.log(this.minhaDataObj);
-  }
+export class AppComponent  {
 }
