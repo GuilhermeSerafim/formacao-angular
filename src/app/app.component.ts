6b23b685eb8bd7f05da607b8e4a3f7c18f589c01
Guilherme Serafim <guilerstudies@gmail.com>
2025-08-09 11:42:40 -0300

    Entendendo diferença entre pipe e função com um exemplo prático

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,11 +14,45 @@ export enum UserStatusEnum {
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit {
-  users!: UsersListResponse;
-  constructor(private readonly _usersServices: UsersService) {}
+export class AppComponent {
+  users = [
+    {
+      name: 'Felipe',
+      status: 1,
+    },
+    {
+      name: 'Laura',
+      status: 2,
+    },
+    {
+      name: 'João',
+      status: 1,
+    },
+    {
+      name: 'Maria',
+      status: 2,
+    },
+  ];
 
-  ngOnInit(): void {
-    this._usersServices.getUsers().subscribe((data) => (this.users = data));
+  inactiveUser(userIndex: number) {
+    this.users[userIndex].status = 2;
+  }
+
+  addUser() {
+    this.users.push({
+      name: 'Marcos',
+      status: 1,
+    });
+  }
+
+  // Dá pra fazer com função, mas ela chama toda vez que o componente muda (como um pipe impuro), o que diminui drasticamente a perfomance da aplicação
+  getUserStatus(userStatus: number): string {
+    console.log('getUserStatus');
+    const status: {[key:string]: string} = {
+      1: 'Ativo',
+      2: 'Inativo'
+    };
+
+    return status[userStatus];
   }
 }
