1d48d9a75ed77bbe80d98fe3c310e3ce53676767
Guilherme Serafim <guilerstudies@gmail.com>
2025-08-11 15:42:17 -0300

    Diminuindo código desnecessário

--- src/app/pipes/filtro-nome.pipe.ts
+++ src/app/pipes/filtro-nome.pipe.ts
@@ -5,9 +5,10 @@ import { IUser } from '../interfaces/user';
   name: 'filtroNome',
 })
 export class FiltroNomePipe implements PipeTransform {
-  transform(users: IUser[] = [], filtro: string = ''): IUser[]{
-    if(!filtro) return users;
-    const alvo = filtro.toLowerCase();
-    return users.filter(u => u.customerName.toLowerCase().includes(alvo));
+  transform(users: IUser[] = [], filtro: string = ''): IUser[] {
+    if (!filtro) return users;
+    return users.filter((u) =>
+      u.customerName.toLowerCase().includes(filtro.toLowerCase())
+    );
   }
 }
