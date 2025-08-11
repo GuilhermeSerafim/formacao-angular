dd8a976a18a114e203bee324ead548fbca5b8042
Guilherme Serafim <guilerstudies@gmail.com>
2025-08-11 10:07:55 -0300

    clean: preparando para desafio envolvendo o que nós aprendemos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,33 +15,5 @@ export enum UserStatusEnum {
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  users = [
-    {
-      name: 'Felipe',
-      status: 3,
-    },
-    {
-      name: 'Laura',
-      status: 2,
-    },
-    {
-      name: 'João',
-      status: 1,
-    },
-    {
-      name: 'Maria',
-      status: 2,
-    },
-  ];
-
-  inactiveUser(userIndex: number) {
-    this.users[userIndex].status = 2;
-  }
-
-  addUser() {
-    this.users.push({
-      name: 'Marcos',
-      status: 1,
-    });
-  }
+  
 }
