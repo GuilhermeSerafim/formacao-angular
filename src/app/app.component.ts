727d3606cb9faef5f495e36296f299383ee5f1a5
Guilherme Serafim <guilerstudies@gmail.com>
2025-08-12 15:25:26 -0300

    Acessando a instancia do form control dentro da classe do componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 export enum UserStatusEnum {
   ATIVO = 1,
@@ -10,6 +17,17 @@ export enum UserStatusEnum {
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  
+export class AppComponent implements AfterViewInit {
+  @ViewChild('meuInputFormControl') inputEl!: NgModel;
+
+  ngAfterViewInit(): void {
+    console.log(this.inputEl);
+  }
+  send() {
+    if (this.inputEl.valid && this.inputEl.touched) {
+      console.log('Enviado com sucesso');
+    } else {
+      console.log('Inválido')
+    }
+  }
 }
