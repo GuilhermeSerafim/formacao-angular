e749290e29459e045d21355c20a946197c94a4b1
GuilhermeSerafim <guilerstudies@gmail.com>
2025-08-31 19:24:17 -0300

    Separando @input e @output do select no ngModel

--- src/app/select/select.component.ts
+++ src/app/select/select.component.ts
@@ -24,4 +24,13 @@ export class SelectComponent {
   ];
 
   nationalitySelected: number | undefined;
+
+  // Aqui eu usaria se caso eu quisesse manipular o que estou recebendo do select
+  onChange(nationality: string) { // É sempre string aqui
+    console.log('onChange nationality', nationality);
+
+    // Esse + antes de nationality é o operador unário de mais em JavaScript e TypeScript.
+    // Ele é usado para converter o valor para número (do tipo number).
+    this.nationalitySelected = +nationality;
+  }
 }
