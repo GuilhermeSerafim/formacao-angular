4f0834b2d659b8bc11ff55455fa0cc99b2995e60
GuilhermeSerafim <guilerstudies@gmail.com>
2025-09-11 12:31:40 -0300

    Entendendo comportamento do ngSubmit com disparamento de botões type submit (default)

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -4,13 +4,18 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrl: './form.component.scss'
+  styleUrl: './form.component.scss',
 })
-export class FormComponent implements AfterViewInit{
+export class FormComponent implements AfterViewInit {
   @ViewChild('meuForm') form!: NgForm;
+  text: string = "Texto inicial";
   ngAfterViewInit(): void {
     console.log(this.form);
-    
   }
-
+  onSubmit(_t5: NgForm) {
+    console.log(_t5);
+    // Fazendo isso, modificamos apenas a propriedade da instância do objeto, mas não afeta no template
+    this.form.value['nome'] = "Texto Alterado";
+    console.log(this.text); // Resultado: "Texto inicial"
+  }
 }
