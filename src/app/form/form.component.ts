08038f4e874bf6da3bb443dafa2f6f5d8b497bbe
GuilhermeSerafim <guilerstudies@gmail.com>
2025-09-11 15:02:47 -0300

    Escutando mudanças no form e em eleemntos especificos - uso de valueChanges

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -8,25 +8,21 @@ import { NgForm } from '@angular/forms';
 })
 export class FormComponent implements AfterViewInit {
   @ViewChild('meuForm') form!: NgForm;
+  @ViewChild('meuInput') input!: NgForm;
   text: string = 'Texto inicial';
   textarea: string = 'Teste';
   select: string = '1';
   radionButton: string = '1';
   checkbox: boolean = true;
   ngAfterViewInit(): void {
-    console.log(this.form);
+    // A cada mudança esse valueChanges dispara (de acordo com o que tiver configurado no updateOn do form)
+    this.form.valueChanges?.subscribe(value => console.log("Form change:", value));
+    this.input.valueChanges?.subscribe(value => console.log("Input change:", value));
   }
   onSubmit(_t5: NgForm) {
-    console.log(_t5.value);
+    console.log("OnSubmit: "+ _t5.value);
   }
   onReset(form: NgForm) {
-    // Comportamento inesperado: eles de fato são resetados no template, mas as props são resetadas depois, portanto, no primeiro momento você pode não esperar esse comportamento
-    form.reset(); // Limpagem manual
-    console.log(form.value);
-    console.log(this.text);
-    console.log(this.textarea);
-    console.log(this.select);
-    console.log(this.radionButton);
-    console.log(this.checkbox);
+    form.reset();
   }
 }
