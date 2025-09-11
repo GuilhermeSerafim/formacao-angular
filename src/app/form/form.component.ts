2cbea5ba85cc480d385af0396bd42d506d846883
GuilhermeSerafim <guilerstudies@gmail.com>
2025-09-11 14:01:16 -0300

    Testando diferentes inputs no ngForm e identificando comportamento inesperado

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -8,14 +8,24 @@ import { NgForm } from '@angular/forms';
 })
 export class FormComponent implements AfterViewInit {
   @ViewChild('meuForm') form!: NgForm;
-  text: string = "Texto inicial";
+  text: string = 'Texto inicial';
+  textarea: string = 'Teste';
+  select: string = '1';
+  radionButton: string = '1';
+  checkbox: boolean = true;
   ngAfterViewInit(): void {
     console.log(this.form);
   }
   onSubmit(_t5: NgForm) {
-    console.log(_t5);
-    // Fazendo isso, modificamos apenas a propriedade da instância do objeto, mas não afeta no template
-    this.form.value['nome'] = "Texto Alterado";
-    console.log(this.text); // Resultado: "Texto inicial"
+    console.log(_t5.value);
+  }
+  onReset(form: NgForm) {
+    // Comportamento inesperado: eles de fato são resetados no template, mas as props são resetadas depois, portanto, no primeiro momento você pode não esperar esse comportamento
+    console.log(form.value);
+    console.log(this.text);
+    console.log(this.textarea);
+    console.log(this.select);
+    console.log(this.radionButton);
+    console.log(this.checkbox);
   }
 }
