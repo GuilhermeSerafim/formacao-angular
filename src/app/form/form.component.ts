aa4b8ca16f0d02d1479d44ee8c9eebb6ae14d059
GuilhermeSerafim <guilerstudies@gmail.com>
2025-09-11 14:02:29 -0300

    Aplicando solução para o comportamento inesperado

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -21,6 +21,7 @@ export class FormComponent implements AfterViewInit {
   }
   onReset(form: NgForm) {
     // Comportamento inesperado: eles de fato são resetados no template, mas as props são resetadas depois, portanto, no primeiro momento você pode não esperar esse comportamento
+    form.reset(); // Limpagem manual
     console.log(form.value);
     console.log(this.text);
     console.log(this.textarea);
