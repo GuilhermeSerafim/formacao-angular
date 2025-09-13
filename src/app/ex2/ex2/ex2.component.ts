750baa4e985f05505e6cce451ca6464f38988a92
GuilhermeSerafim <guilerstudies@gmail.com>
2025-09-13 17:40:30 -0300

    Aplicando a customização do estado de exibição no nosso componente inteiro atraves de providers

--- src/app/ex2/ex2/ex2.component.ts
+++ src/app/ex2/ex2/ex2.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { CustomErrorStateMatcher } from '../utils/custom-error-state.matcher';
+import { ErrorStateMatcher } from '@angular/material/core';
 
 @Component({
   selector: 'app-ex2',
   templateUrl: './ex2.component.html',
   styleUrl: './ex2.component.scss',
+  providers: [
+    // Sempre que alguém tentar instanciar a classe ErroStateMatcher neste componente
+    // Na real, vai pegar a classe CustomErrorStateMatcher, dessa forma, pegará nossa personalização de quando irá exibir o erro
+    { provide: ErrorStateMatcher, useClass: CustomErrorStateMatcher },
+  ],
 })
 export class Ex2Component {
   matcher = new CustomErrorStateMatcher();
