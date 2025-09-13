9da5e2aa47894994cd52617d59a56361493cb657
GuilhermeSerafim <guilerstudies@gmail.com>
2025-09-13 17:41:31 -0300

    Aplicando a customização do estado de exibição na nossa aplicação inteira atraves de providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,26 @@ import localePt from '@angular/common/locales/pt';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MatRippleModule } from '@angular/material/core';
+import { ErrorStateMatcher, MatRippleModule } from '@angular/material/core';
 
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { MatInputModule } from '@angular/material/input';
 import { Ex1Component } from './ex1/ex1/ex1.component';
 import { Ex2Component } from './ex2/ex2/ex2.component';
 import { EmailValidatorDirective } from './ex2/email-validator.directive';
+import { CustomErrorStateMatcher } from './ex2/utils/custom-error-state.matcher';
 
 registerLocaleData(localePt, 'pt-BR');
 
 @NgModule({
-  declarations: [AppComponent, Ex1Component, Ex2Component, EmailValidatorDirective],
+  declarations: [
+    AppComponent,
+    Ex1Component,
+    Ex2Component,
+    EmailValidatorDirective,
+  ],
   imports: [
     BrowserModule,
     FormsModule,
@@ -33,6 +39,7 @@ registerLocaleData(localePt, 'pt-BR');
   providers: [
     { provide: LOCALE_ID, useValue: 'pt-BR' },
     { provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' },
+    { provide: ErrorStateMatcher, useClass: CustomErrorStateMatcher },
     provideHttpClient(),
     provideAnimationsAsync(),
   ],
--- src/app/ex2/ex2/ex2.component.ts
+++ src/app/ex2/ex2/ex2.component.ts
@@ -7,9 +7,6 @@ import { ErrorStateMatcher } from '@angular/material/core';
   templateUrl: './ex2.component.html',
   styleUrl: './ex2.component.scss',
   providers: [
-    // Sempre que alguém tentar instanciar a classe ErroStateMatcher neste componente
-    // Na real, vai pegar a classe CustomErrorStateMatcher, dessa forma, pegará nossa personalização de quando irá exibir o erro
-    { provide: ErrorStateMatcher, useClass: CustomErrorStateMatcher },
   ],
 })
 export class Ex2Component {
