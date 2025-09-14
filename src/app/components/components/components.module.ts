782c43d5be87b1fc45366079a0558bff6c42fe2d
GuilhermeSerafim <guilerstudies@gmail.com>
2025-09-14 17:33:13 -0300

    Criação do componente UsersCardsListComponent

--- src/app/components/components/components.module.ts
+++ src/app/components/components/components.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AngularMaterialModule } from '../../angular-material/angular-material.module';
 import { DirectivesModule } from '../../directives/directives/directives.module';
+import { UsersCardListComponent } from '../users-card-list/users-card-list.component';
 
 // declarations → coisas que pertencem ao módulo (componentes, pipes, diretivas).
 // imports → módulos externos que você precisa usar aqui.
 // exports → coisas desse módulo que você quer disponibilizar para outros.
 @NgModule({
-  declarations: [],
+  declarations: [UsersCardListComponent],
   imports: [CommonModule, FormsModule, AngularMaterialModule, DirectivesModule],
-  exports: [],
+  exports: [UsersCardListComponent],
 })
 export class ComponentsModule {}
