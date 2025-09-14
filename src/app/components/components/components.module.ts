e3a76eb01ed46978cc493321cc3f89cb49e4eb7c
GuilhermeSerafim <guilerstudies@gmail.com>
2025-09-14 15:30:08 -0300

    Criação e estruturação dos módulos

---
+++ src/app/components/components/components.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AngularMaterialModule } from '../../angular-material/angular-material.module';
+import { DirectivesModule } from '../../directives/directives/directives.module';
+
+// declarations → coisas que pertencem ao módulo (componentes, pipes, diretivas).
+// imports → módulos externos que você precisa usar aqui.
+// exports → coisas desse módulo que você quer disponibilizar para outros.
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, FormsModule, AngularMaterialModule, DirectivesModule],
+  exports: [],
+})
+export class ComponentsModule {}
