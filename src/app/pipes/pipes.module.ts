d53bfc6219246ac1a659a4b8de9080686c00d9e5
GuilhermeSerafim <guilerstudies@gmail.com>
2025-09-22 15:37:05 -0300

    Criando pipe para exibição da musica favorita

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { GenreDescriptionPipe } from './genre-description.pipe';
 import { StatesDescriptionPipe } from './states-description.pipe';
+import { IsFavoriteDescriptionPipe } from './is-favorite-description.pipe';
 
 @NgModule({
-  declarations: [GenreDescriptionPipe, StatesDescriptionPipe],
-  exports: [GenreDescriptionPipe, StatesDescriptionPipe],
+  declarations: [GenreDescriptionPipe, StatesDescriptionPipe, IsFavoriteDescriptionPipe],
+  exports: [GenreDescriptionPipe, StatesDescriptionPipe, IsFavoriteDescriptionPipe],
 })
 export class PipesModule {}
